12dff42d63dd3d0ee569117bdcdeec7cc9d559ea
Ian <ianaanderson2011@gmail.com>
2019-07-07 16:20:36 +0000

    Testing push

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authentication_user!, only: [:new, :create]
 
   def index 
       @places = Place.all.page(params[:page]).per_page(5)
