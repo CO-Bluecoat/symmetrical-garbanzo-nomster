59c07e064d87041612841ce256f873bef39fef82
Ian <ianaanderson2011@gmail.com>
2019-08-13 04:36:23 +0000

    Added new place form that can be edited

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,33 @@
 class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  def index 
-      @places = Place.all.page(params[:page]).per_page(5)
- end
+  def index
+    @places = Place.paginate(:page => params[:page], :per_page => 10)
+  end
 
   def new
-      @place = Place.new
+    @place = Place.new
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  def edit
   end
 
   private
 
-  def create
-  current_user.places.create(place_params)
-  redirect_to root_path
-end
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
