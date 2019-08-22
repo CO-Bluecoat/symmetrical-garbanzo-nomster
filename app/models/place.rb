df5020d37f2f541602178afbc733f538c2793ae0
Ian <ianaanderson2011@gmail.com>
2019-08-22 03:18:26 +0000

    added maps

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
+  validates :description, presence: true 
+  validates :address, presence: true
+  validates :name, presence: true, length: {minimum: 3} 
+ 
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
